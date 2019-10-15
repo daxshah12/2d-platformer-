ea4decf97ca3462ea9117e8392d4d4f9b26b0c2e
daxshah12 <43219335+daxshah12@users.noreply.github.com>
2019-10-15 12:38:15 -0400

    Add files via upload

---
+++ Bullets.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour {
+
+	private float timer;
+
+	void Update()
+    { 
+	timer += 1.0f * Time.deltaTime;
+
+		if (timer >= 2.0f)
+		{
+			GameObject.Destroy(gameObject);
+		}
+	}
+	private void OnCollisionEnter2D(Collision2D col)
+	{
+		
+		if (col.gameObject.tag == "Enemy")
+		{
+			Destroy(col.gameObject);
+			Destroy(gameObject);
+		}
+
+	}
+}
---
+++ EnemyDeath.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+		Debug.Log("Collider with enemy");
+		if (col.gameObject.tag == "Player")
+		{
+			//Destroy(col.gameObject);
+			HUD.health -= 1;
+		}
+        
+    }
+}
